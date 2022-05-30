a28019829981057c63c97715e025c2cb9ef01235
FlutLab.io <>
2022-05-30 04:03:44 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BelajarNavBar(),
+    );
+  }
+}
+
+class BelajarNavBar extends StatefulWidget {
+  @override
+  _BelajarNavBarState createState() => _BelajarNavBarState();
+}
+
+class _BelajarNavBarState extends State<BelajarNavBar> {
+  int _selectedNavbar = 0;
+  var _pages = <Widget>[
+    Icon(Icons.home, size: 250, color: Colors.blueGrey),
+    Icon(Icons.assignment, size: 250, color: Colors.blueGrey),
+    Icon(Icons.person, size: 250, color: Colors.blueGrey),
+    Icon(Icons.mail, size: 250, color: Colors.blueGrey),
+  ];
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Bottom Navbar"),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Center(child: _pages.elementAt(_selectedNavbar)),
+        color: Colors.black,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: ('Beranda'),
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: ('Pesanan'),
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: ('Akun'),
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: ('Inbox'),
+            backgroundColor: Colors.black,
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
+      ),
+    );
+  }
+}
